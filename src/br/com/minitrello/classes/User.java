2ddf1aeeebf81958386900263451cef9943975f6
iuxie <iurexavier10@gmail.com>
2026-06-13 01:10:30 -0300

    feat: add main and user class

---
+++ src/br/com/minitrello/classes/User.java
@@ -0,0 +1,39 @@
+package br.com.minitrello.classes;
+
+import java.util.UUID;
+
+public class User {
+
+    // TODO: Implementar atributo de List<Dashboard> quando a classe for criada.
+    private final UUID id;
+    private String name;
+    private final String email, password;
+
+    public User(String name, String email, String password) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
